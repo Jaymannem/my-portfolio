0b304ed9c6eaa00ed0721efc63aa17394a0a3e27
Jaymannem <jaymannem21@gmail.com>
2025-07-05 12:39:56 +0530

    home and navbar changes

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <a>Home</a>
+        <a>About me</a>
+        <a>Skills</a>
+        <a>Contact</a>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
