ae1dfd4193780023c5f73ef4c9220f87822c1c88
Jaymannem <jaymannem21@gmail.com>
2025-07-05 12:53:03 +0530

    removed navbar changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import Navbar from "./Components/Navbar/Navbar";
-
 function App() {
   return (
     <div>
-      <Navbar />
       <h1 className="text-3xl font-bold underline">Jaya Subbareddy</h1>
     </div>
   );
